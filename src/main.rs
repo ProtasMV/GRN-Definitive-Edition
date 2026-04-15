9fb979ffb12010a9dfdc46fdbcce6b0eb26fb15a
ProtasMV <160389679+ProtasMV@users.noreply.github.com>
2026-04-15 14:22:07 +0300

    Refactor game commands and improve messages
    Обновление 1.0.1 | Список изменений:
    1. Исправлены грамматические ошибки 
    2. Небольшое улучшение архитектуры кода 
        [добавлены константы]
    3. Обновлено описание проекта

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ use rand::Rng;
 use colored::*;
 
 fn main() {
+    const LATUTA_EASTER: &str = "/latuta";
+    const RICHARD_EASTER: &str = "/richard";
+    const STOP_COMAND: &str = "/stop";
+    const CHEATS_COMAND: &str = "/cheats";
+    const RANDOM_MIN: u32 = 1;
+    const EASY_RANDOM_MAX: u32 = 50;
+    const NORMAL_RANDOM_MAX: u32 = 100;
+    const HARD_RANDOM_MAX: u32 = 1000;
+    const UNPOSIBLE_RANDOM_MAX: u32 = 10000;
+    
     let mut trying = 0;
     let mut mode = String::new();
     let mut tips_off = 0;
@@ -32,7 +42,7 @@ fn main() {
 |   |_| ||   |\\\\ | | |   |
 |_______||___| \\\\|_|  |__|
     ".purple().bold());
-    println!("{}", "Добро пожаливать в GRN Definitive Edition!".purple().bold());  
+    println!("{}", "Добро пожаловать в GRN Definitive Edition!".purple().bold());  
     'start: loop {
         println!("{}", "1)Играть, 2)Описание".blue());
 
@@ -43,19 +53,19 @@ fn main() {
         if setting_or_game.trim() == "2"||setting_or_game.trim().to_lowercase() == "two"||setting_or_game.trim().to_lowercase() == "описание"{
             println!("
 Суть данной игры — улучшить оригинальную 'Угадайку' [Guessing Game] из официальной обучающей
-книги по Rust (The Rust Book). В то время как оригинальный код занимает около 30 строк,
- версия Definitive Edition содержит 327 строк");
+книги по Rust [The Rust Book]. В то время как оригинальный код занимает около 30 строк,
+ версия Definitive Edition содержит 325+ строк");
             println!(" ");
             println!("
-Так же в игре присудствуют команды их можно ввести почти в любом доступном для ввода месте,
-что бы начать писать команду нужно ввести [/], в игре на данный момент
-приствуют перечисленые команды: /stop, /cheats");
+Также в игре присудствуют команды. Их можно ввести почти в любом месте,
+чтобы начать ввод команды, используйте символ [/].
+На данный момент доступны следующие команды: /stop, /cheats");
             println!(" ");
             println!("
-Однозначно, игра также содержит пасхалки ввести их можно спомощью команд через [/], на данный момент
-присутвуют перечисленные пасхальние команды: /richard, /latuta");
+Конечно же, в игре есть пасхалки. Активировать их можно с помощью команд через [/]. 
+На данный момент доступны: /richard, /latuta.");
             println!("");
-            println!("Нажмите любую кнопку, для возращения в главное меню");
+            println!("Нажмите любую клавишу для возвращения в главное меню");
 
             setting_or_game.clear();
             io::stdin()
@@ -68,8 +78,8 @@ fn main() {
             } else if setting_or_game.trim() == "1" || setting_or_game.trim().to_lowercase() == "one" || setting_or_game.trim().to_lowercase() == "играть"{
                     loop {
                 println!("{}", "На какой сложности вы бы хотели сыграть?".blue()); 
-                println!("{}", "1)Лёгкая [1-50] 2)Нормальная [1-100], 3)Сложная [1-1000]".green());     
-                println!("{}", "4)Невозможная [1-10000] + нету подсказок больше/меньше, 5)Кастомная [самостоятельно указать диапазон]".green());
+                println!("{}", "1) Лёгкая [1-50], 2) Нормальная [1-100], 3) Сложная [1-1000],".green());     
+                println!("{}", "4)Невозможная [1-10000] + нет подсказок больше/меньше, 5) Кастомная [самостоятельно указать диапазон]".green());
 
                 io::stdin()
                     .read_line(&mut mode)
@@ -81,20 +91,20 @@ fn main() {
                     "3" => break,
                     "4" => break,
                     "5" => {castom = 1; break},
-                    "/stop" => break 'start,
-                    "/cheats" => {
+                    STOP_COMAND => break 'start,
+                    CHEATS_COMAND => {
                         setting_or_game.clear();
                         mode.clear();
                         if cheats_on == 0 {
-                            cheats_on = 1; println!("Читы успешно активируваные"); continue;
+                            cheats_on = 1; println!("Читы успешно активированы"); continue;
                         }else if cheats_on == 1{
-                            cheats_on = 0; println!("Читы успешно выключенны"); continue; 
+                            cheats_on = 0; println!("Читы успешно выключены"); continue; 
                         }else {
-                            println!("Error 20")
+                            println!("Error 22")
                         }
                     }
-                    "/latuta" => {setting_or_game.clear(); println!("Латута тварь"); continue;}
-                    "/richard" => {setting_or_game.clear(); println!("Ну он крутой"); continue;}
+                    LATUTA_EASTER => {setting_or_game.clear(); println!("Латута тварь"); continue;}
+                    RICHARD_EASTER => {setting_or_game.clear(); println!("Ну он крутой"); continue;}
                     &_ => continue
                 }
                 
@@ -116,7 +126,7 @@ fn main() {
 
                         castom_one = match castom_num_one.trim().parse() {
                             Ok(user_num) => user_num,
-                            Err(_) => {println!("Error 10, подсказка: возможно вы написали данные типа str, попробуйте ответить только числами"); continue;}  
+                            Err(_) => {println!("Error 10, подсказка: возможно, вы ввели данные типа str, попробуйте ввести только числа"); continue;}  
                         };
                         break;
                     };
@@ -131,11 +141,11 @@ fn main() {
 
                     castom_two = match castom_num_two.trim().parse() {
                         Ok(user_num) => user_num,
-                        Err(_) => {println!("Error 12, подсказка: возможно вы написали данные типа str, попробуйте ответить только числами"); continue;}                    
+                        Err(_) => {println!("Error 12, подсказка: возможно, вы ввели данные типа str, попробуйте ввести только числа"); continue;}                    
                     };
 
                     if castom_two < castom_one {
-                        println!("Error 13, подсказка: большое число не должно бить меньше меньшего"); castom_num_two.clear(); continue; 
+                        println!("Error 13, подсказка: большее число не должно быть меньше меньшего"); castom_num_two.clear(); continue; 
                     }
                     break;
                 }
@@ -178,40 +188,40 @@ fn main() {
                     .read_line(&mut user_guess)
                     .expect("Error 1");
 
-                if user_guess.trim().to_lowercase() == "/stop"{
+                if user_guess.trim().to_lowercase() == STOP_COMAND{
                     break 'start;
-                } else if user_guess.trim().to_lowercase() == "/cheats" {
+                } else if user_guess.trim().to_lowercase() == CHEATS_COMAND {
                     user_guess.clear();
                     
                     if cheats_on == 0 {
-                        cheats_on = 1; println!("Читы успешно активируваные"); continue;
+                        cheats_on = 1; println!("Читы успешно активированы"); continue;
                     }else if cheats_on == 1{
-                        cheats_on = 0; println!("Читы успешно выключенны"); continue; 
+                        cheats_on = 0; println!("Читы успешно выключены"); continue; 
                     }else {
-                        println!("Error 20")
+                        println!("Error 21")
                     }
-                } else if user_guess.trim().to_lowercase() == "/latuta" {
+                } else if user_guess.trim().to_lowercase() == LATUTA_EASTER {
                     user_guess.clear();
                     println!("Латута тварь");
                     continue;
-                } else if user_guess.trim().to_lowercase() == "/richard" {
+                } else if user_guess.trim().to_lowercase() == RICHARD_EASTER {
                     println!("Он крутой");
                     continue;
                 }
 
                 let user_guess: u32 = match user_guess.trim().parse() {
                     Ok(num) => num,
-                    Err(_) => {println!("Error 2, Ошибка ввода, подсказка: водд не потдержует текстовый формат"); continue;} 
+                    Err(_) => {println!("Error 2, ошибка ввода, подсказка: ввод не поддерживает текстовый формат"); continue;} 
                 };
 
-                if mode.trim() == "1" && user_guess > 50 || mode.trim() == "1" && user_guess < 1{
-                    println!("{}","Ей, вообще-то мы договаривались играть в радиусе [1-50], попытка не засчитана!".yellow());
-                } else if mode.trim() == "2" && user_guess > 100 || mode.trim() == "2" && user_guess < 1{
-                    println!("{}","Ей, вообще-то мы договаривались играть в радиусе [1-100], попытка не засчитана!".yellow());                
-                } else if mode.trim() == "3" && user_guess > 1000 || mode.trim() == "3" && user_guess < 1{
-                    println!("{}","Ей, вообще-то мы договаривались играть в радиусе [1-1000], попытка не засчитана!".yellow());
-                } else if mode.trim() == "4" && user_guess > 10000 || mode.trim() == "4" && user_guess < 1{
-                    println!("{}","Ей, вообще-то мы договаривались играть в радиусе [1-10000], попытка не засчитана!".yellow());
+                if mode.trim() == "1" && user_guess > EASY_RANDOM_MAX || mode.trim() == "1" && user_guess < RANDOM_MIN{
+                    println!("{}","Эй, вообще-то мы договаривались играть в диапазоне [1-50], попытка не засчитана!".yellow());
+                } else if mode.trim() == "2" && user_guess > NORMAL_RANDOM_MAX || mode.trim() == "2" && user_guess < RANDOM_MIN{
+                    println!("{}","Эй, вообще-то мы договаривались играть в диапазоне [1-100], попытка не засчитана!".yellow());                
+                } else if mode.trim() == "3" && user_guess > HARD_RANDOM_MAX || mode.trim() == "3" && user_guess < RANDOM_MIN{
+                    println!("{}","Эй, вообще-то мы договаривались играть в диапазоне [1-1000], попытка не засчитана!".yellow());
+                } else if mode.trim() == "4" && user_guess > UNPOSIBLE_RANDOM_MAX || mode.trim() == "4" && user_guess < RANDOM_MIN{
+                    println!("{}","Эй, вообще-то мы договаривались играть в диапазоне [1-10000], попытка не засчитана!".yellow());
                 }else if mode.trim() == "5" && user_guess > castom_two || mode.trim() == "5" && user_guess < castom_one{
                     print!("{}", "Ей, вообще-то мы договаривались играть в радиусе ".yellow());
                     print!("[{}-{}]", castom_one, castom_two);
@@ -240,9 +250,9 @@ fn main() {
                         if trying<=0 {
                             println!("{}", "Ты выиграл! Каким образом!?".red().bold());
                         } else if trying==1 {
-                            println!("{}", "Ты выиграл! Даже не знаю как тебе это удалось!".purple().bold())
+                            println!("{}", "Ты выиграл! Даже не знаю, как тебе это удалось!".purple().bold())
                         } else if trying>1 && trying<=5 {
-                            println!("{}", "Ты выиграл! Ричард горд твоим майтерством!".purple().bold())
+                            println!("{}", "Ты выиграл! Ричард горд твоим мастерством!".purple().bold())
                         } else if trying>5 && trying <=15 {
                             println!("{}", "Ты выиграл! Молодец!".green().bold())
                         } else if trying==994 || trying==993 {
@@ -251,7 +261,7 @@ fn main() {
                             println!("{}", "Ты выиграл!".green().bold())                        
                         }
 
-                        print!("{}", "Угадано спутся попыток: ".blue());
+                        print!("{}", "Угадано спустя попыток: ".blue());
                         println!("{}", trying); 
 
                         if best_trying_one > best_trying_two {
@@ -261,7 +271,7 @@ fn main() {
                         }
                         
                         if best_result_time >= 2 {
-                            print!("{}", "Твой лучший результат в етой сессии: ".yellow());
+                            print!("{}", "Твой лучший результат в этой сессии: ".yellow());
                             println!("{}", best_trying_duo);
                         }
                         
@@ -278,22 +288,22 @@ fn main() {
                                 trying = 0; break 'game;
                             } else if user_continue.trim().to_lowercase() == "нет" || user_continue.trim().to_lowercase() == "n" {
                                 break 'start;
-                            } else if user_continue.trim().to_lowercase() == "/stop" {
+                            } else if user_continue.trim().to_lowercase() == STOP_COMAND {
                                 break 'start;
-                            } else if user_continue.trim().to_ascii_lowercase() == "/cheats"{
+                            } else if user_continue.trim().to_ascii_lowercase() == CHEATS_COMAND{
                                 user_continue.clear();
                                 if cheats_on == 0 {
-                                    cheats_on = 1; println!("Читы успешно активируваные"); continue;
+                                    cheats_on = 1; println!("Читы успешно активированы"); continue;
                                 }else if cheats_on == 1{
-                                    cheats_on = 0; println!("Читы успешно выключенны"); continue; 
+                                    cheats_on = 0; println!("Читы успешно выключены"); continue; 
                                 }else {
                                     println!("Error 20")
                                 }                          
-                            } else if user_continue.trim().to_lowercase() == "/latuta" {
+                            } else if user_continue.trim().to_lowercase() == LATUTA_EASTER {
                                 setting_or_game.clear();
                                 println!("Латута тварь");
                                 continue;                               
-                            } else if user_continue.trim().to_lowercase() == "/richard"  {
+                            } else if user_continue.trim().to_lowercase() == RICHARD_EASTER  {
                                 setting_or_game.clear();
                                 println!("Он крутой");  
                                 continue;                          
@@ -306,27 +316,27 @@ fn main() {
             }
         }
     }
-        } else if setting_or_game.trim().to_lowercase() == "/stop"{
+        } else if setting_or_game.trim().to_lowercase() == STOP_COMAND{
             break 'start;
-        } else if setting_or_game.trim().to_lowercase() == "/cheats"{
+        } else if setting_or_game.trim().to_lowercase() == CHEATS_COMAND{
             setting_or_game.clear();
             if cheats_on == 0 {
-                cheats_on = 1; println!("Читы успешно активируваные"); continue;
+                cheats_on = 1; println!("Читы успешно активированы"); continue;
             }else if cheats_on == 1{
-                cheats_on = 0; println!("Читы успешно выключенны"); continue; 
+                cheats_on = 0; println!("Читы успешно выключены"); continue; 
             }else {
                 println!("Error 19")
             }
-        } else if setting_or_game.trim().to_lowercase() == "/latuta" {
+        } else if setting_or_game.trim().to_lowercase() == LATUTA_EASTER {
             setting_or_game.clear();
             println!("Латута тварь");
             continue;
-        } else if setting_or_game.trim().to_lowercase() == "/richard"   {
+        } else if setting_or_game.trim().to_lowercase() == RICHARD_EASTER   {
             setting_or_game.clear();
             println!("Он крутой");
             continue;
         } else {
-            println!("Error 18, подсказка: програме не удалось использывать ваш водд, попробуйте еще раз"); setting_or_game.clear(); continue;        
+            println!("Error 18, подсказка: программе не удалось использовать ваш ввод, попробуйте ещё раз"); setting_or_game.clear(); continue;        
         }
     } 
 }
