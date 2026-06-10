fe126b54ae85f35312d828f9b3c8cd9a7b32e6a3
ProtasMV <protasmv@gmail.com>
2026-06-10 18:35:20 +0300

    1.1.0 | Dev-beta 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::{cmp::Ordering, io::{self, Write}};
 use rand::random_range;
+use colored::*;
+
+const SYS_COLOR: Color = Color::TrueColor { r: 244, g: 208, b: 63 };
+const BG_COLOR: Color = Color::TrueColor { r: 160, g: 135, b: 40 };
+const CORRECT_COLOR: Color = Color::TrueColor { r: 57, g: 135, b: 11 };
+const WRONG_COLOR: Color = Color::Red;
 
 fn main() {
-    println!("
+    println!("{}", "
  _______  ______  __    _ 
 |       ||    _ ||  |  | |
 |    ___||   | |||   |_| |
@@ -10,13 +16,16 @@ fn main() {
 |   ||  ||    __||  _    |
 |   |_| ||   |\\\\ | | |   |
 |_______||___| \\\\|_|  |__|
-    ");
-
-    println!("Welcome to Guess Random Number DFE!");
+    ".color(SYS_COLOR));
+    
+    println!("{}", "Welcome to Guess Random Number DFE!".color(SYS_COLOR));
+    println!("{}", "Made by: @ProtasMV".color(BG_COLOR));
+    println!();
+    
     loop {
-        println!("1)Play, 2)Description");
+        println!("{}", "1)Play, 2)Description".color(SYS_COLOR));
 
-        print!("Mode: ");
+        print!("{}", "Mode: ".color(BG_COLOR));
         flush();
 
         let (user_choise, error) = input();
@@ -24,10 +33,11 @@ fn main() {
 
         match user_choise.trim().to_lowercase().as_str() {
             "1"|"play" => {
-                main_game();
+                rand_num();
+                break;
             },
             "2"|"description"|"des" => {
-
+                description();
             }
             _=> {
                 println!("Invalid input, please try again");
@@ -37,30 +47,87 @@ fn main() {
     }
 }
 
-fn main_game() {
-    loop {
-        let rand_number = random_range(1..500);
+fn rand_num() {   
+    'reset: loop {
+        let mut best_attempt = 4294967295;
         
+        for round in 1..999999999 {
+            let rand_number = random_range(1..=500);
+            let attempt: u32 = 0; 
+            
+            let (error, attempt) = main_game(rand_number, attempt);
+            if error {break 'reset}
+
+            if attempt < best_attempt {
+                best_attempt = attempt
+            }
+
+            win(attempt, round, best_attempt);
+            if !user_continue() {break 'reset};
+        }
+    }
+}
+
+fn main_game(rand_number: i32, mut attempt: u32) -> (bool, u32)  { 
+    loop {
         println!();        
-        print!("Enter your guess: ");
+        
+        //cheats
+        print!("{}", "CHEATS: ".color(BG_COLOR));
+        println!("{rand_number}");
+        
+        print!("{}", "Enter your guess: ".color(SYS_COLOR));
         flush();
         let (user_try, error) = input();
-        if error {break}
+        if error {break (true, 0)}
 
         let (user_try, error) = parse_to_i32(user_try);
         if error {continue}
 
         match user_try.cmp(&rand_number) {
-            Ordering::Less => {println!("Bigger"); continue},
-            Ordering::Greater => {println!("Smaller"); continue},
-            Ordering::Equal => win()
+            Ordering::Less => {println!("{}", "Bigger".color(WRONG_COLOR)); attempt += 1; continue},
+            Ordering::Greater => {println!("{}", "Smaller".color(WRONG_COLOR)); attempt += 1; continue},
+            Ordering::Equal => {        
+                attempt += 1;
+                break (false, attempt);
+            }
         }
     }
 }
 
-fn win() {
-    println!("");
-    println!("You win!")
+fn win(attempt: u32, round: u32, best_attempt: u32) {
+    println!();
+    println!("{}", "You win!".color(CORRECT_COLOR)); 
+
+    if round >= 2 { 
+        print!("{}", "Best score: ".color(SYS_COLOR));
+        println!("{best_attempt}");
+    }
+
+    print!("{}", "Attempt: ".color(BG_COLOR));
+    println!("{attempt}");
+}
+
+fn user_continue() -> bool {
+    loop {
+        println!();
+        print!("{}", "Continue? (Y/N): ".color(SYS_COLOR));
+        flush();
+
+        let (user_want, error) = input();
+        if error {return false}
+
+        match user_want.trim().to_lowercase().as_str() {
+            "1"|"y"|"yes" => break true,
+            "2"|"n"|"no" => break false,
+            _=> println!("Invalid input, please try again")
+        }
+    }
+}
+
+fn description() {
+    println!("Not available in dev-beta");
+    println!();
 }
 
 fn input() -> (String, bool) {
